38a46cf43656a211d4055ab831028b969b3cae3b
igarashi339 <igarashi339@gmail.com>
2019-06-29 17:27:15 +0900

    [add] エントリポイントのプログラムを追加

---
+++ exec/exec.py
@@ -0,0 +1,16 @@
+from src.model.instance import Instance
+from src.util.io import IO
+from src.algorithm.random_solver import RandomSolver
+
+# 問題の読み取り
+io = IO()
+io.input_problem()
+instance = Instance()
+instance.set_stamp_object(io.stamp_object_list)
+
+# ソルバーの生成 & 解の計算
+solver = RandomSolver() # ここを切り替えることによって実行するアルゴリズムを変更できる
+solution = solver.calc_soltion()
+
+# 解の出力
+io.output_solution(solution)
