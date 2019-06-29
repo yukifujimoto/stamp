d8e2448932dfbd483a773c42a94d07fcfc4906b3
igarashi339 <igarashi339@gmail.com>
2019-06-29 17:20:32 +0900

    [add] solution.py を追加

---
+++ src/model/solution.py
@@ -0,0 +1,8 @@
+from src.model.field import Field
+from src.model.stamp import Stamp
+from src.model.instance import Instance
+from src.util.io import IO
+
+class Solution:
+    def __init__(self):
+        pass
