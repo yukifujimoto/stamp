f6bccff160c49c35b95abcc9e7370ebf188ce59b
yukifijimoto <pwjnc853@gmail.com>
2019-07-13 17:41:44 +0900

    [modify]IOクラスのインポートパス変更

--- src/model/solution.py
+++ src/model/solution.py
@@ -3,6 +3,40 @@ from src.model.stamp import Stamp
 from src.model.instance import Instance
 from src.util.io import IO
 
+
 class Solution:
+    """
+    解（スタンプをどこに押すか）を保持するクラス
+
+    Attributes
+    ----------
+    """
+
     def __init__(self):
-        pass
+        """
+        コンストラクタ。
+        """
+
+        self.stamp_answer_list = []
+
+    def add_stamp_answer(self, id, x, y):
+        """
+        stamp_answer_listにsスタンプを押す座標情報を追加する。
+        Parameters
+    　　----------
+    　　stamp_object : Stamp
+            Stampクラスのオブジェクト。
+        """
+
+        self.stamp_answer_list(tuple(id, x, y))
+
+    def get_stamp_answer_list(self):
+        """
+        stamp_answer_listに解スタンプ位置情報を追加する。
+        Parameters
+    　　----------
+    　　stamp_object : Stamp
+            Stampクラスのオブジェクト。
+        """
+
+        return self.stamp_answer_list
