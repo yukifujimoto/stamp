2c2fe154e7024c8a05350d9c2eedd0cfe839f37f
yukifijimoto <pwjnc853@gmail.com>
2019-08-04 13:03:54 +0900

    [add]分割アルゴリズム

--- exec/exec.py
+++ exec/exec.py
@@ -1,6 +1,7 @@
 from src.model.instance import Instance
 from src.util.io import IO
 from src.algorithm.random_solver import RandomSolver
+from src.algorithm.random_solver_devide_field import RandomSolverDevideField
 
 # 問題の読み取り
 io = IO()
@@ -9,24 +10,12 @@ instance = Instance()
 instance.set_stamp_object(io.stamp_object_list)
 
 # ソルバーの生成 & 解の計算
-solver = RandomSolver() # ここを切り替えることによって実行するアルゴリズムを変更できる
+#solver = RandomSolver() # ここを切り替えることによって実行するアルゴリズムを変更できる
+solver = RandomSolverDevideField() # ここを切り替えることによって実行するアルゴリズムを変更できる
 solution = solver.calc_solution(instance)
 
 # 解の出力
 io.output_solution(solution)
 
 if __name__ == "__main__":
-    '''
-    # 問題の読み取り
-    io = IO()
-    io.input_problem()
-    instance = Instance()
-    instance.set_stamp_object(io.stamp_object_list)
-
-    # ソルバーの生成 & 解の計算
-    solver = RandomSolver()  # ここを切り替えることによって実行するアルゴリズムを変更できる
-    solution = solver.calc_soltion(instance)
-
-    # 解の出力
-    io.output_solution(solution)
-    '''
+    pass
